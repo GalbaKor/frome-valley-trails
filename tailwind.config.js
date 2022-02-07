8697b74045e1e8e45fcdefcdaf550ebad8bf77e8
GalbaKor <cameron.tebbenham@outlook.com>
2022-02-07 20:08:34 +0000

    route 3 experimentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     "./public/route-home.html",
     "./public/routes/*.html",
+    "./public/routes/route-3-points/*.html",
+    "./public/routes/**/*.html",
     "./src/**/*.{html,js}",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
