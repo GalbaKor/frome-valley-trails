aeb25b7297c1f767119fba9adc7bbabced662732
GalbaKor <cameron.tebbenham@outlook.com>
2022-02-28 14:51:26 +0000

    First point quiz is functional

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  content: [
-    // "./public/route-home.html",
-    // "./public/routes/*.html",
-    // "./public/routes/route-3-points/*.html",
-    // "./public/routes/**/*.html",
-    // "./src/**/*.{html,js}",
-    // "./node_modules/tw-elements/dist/js/**/*.js",
-    "./public/**/*.{html,js}",
-  ],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
